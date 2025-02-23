e8617d07f14ecf72e38cb5e313702299ceaa3771
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-02-23 12:47:34 -0300

    fix: changing layout from panel

--- RH/src/Routes/Painel.tsx
+++ RH/src/Routes/Painel.tsx
@@ -1,40 +1,15 @@
-import { FunctionComponent } from "react"
+import { FunctionComponent} from "react"
 import { BsBriefcaseFill } from "react-icons/bs";
+import { SideBar } from "../components/SideBar";
 
 export const Painel:FunctionComponent = () =>{
+
     return(
         <body className="flex h-screen bg-slate-100">
-        <aside className="flex flex-col w-1/6 items-center p-3 rounded-r-xl bg-white">
-            <ul >
-                <ul>                    
-                <li>Recrutamento e seleção</li>
-                </ul>
-                <ul>
-                <li>Departamento pessoal</li>
-                </ul>
-            </ul>
-        </aside>
-        <main className="w-full p-32 flex flex-wrap gap-5">
-            <div className="flex flex-col bg-white w-80 h-40 rounded-lg p-5 items-center justify-center">
-            <p className="text-5xl"><BsBriefcaseFill/> </p>
-               <h1 className="text-2xl">Gestão de Vagas</h1>
-            </div>
+        <SideBar/>
+        <main className="w-full p-20 flex flex-wrap gap-5">
+            <div className="bg-white w-full h-full rounded-lg">
 
-            
-            <div className="flex flex-col bg-white w-80 h-40 rounded-lg p-5 items-center justify-center">
-            <p className="text-5xl"><BsBriefcaseFill/> </p>
-               <h1 className="text-2xl">Triagem e Avaliação</h1>
-            </div>
-
-            <div className="flex flex-col bg-white w-80 h-40 rounded-lg p-5 items-center justify-center">
-            <p className="text-5xl"><BsBriefcaseFill/> </p>
-               <h1 className="text-2xl">Fluxo de Seleção</h1>
-            </div>
-
-            <div className="flex flex-col bg-white w-80 h-40 rounded-lg p-5 items-center justify-center">
-            <p className="text-5xl"><BsBriefcaseFill/> </p>
-               <h1 className="text-2xl">Relatórios e Dashboards
-               </h1>
             </div>
 
 
---
+++ RH/src/components/SideBar.tsx
@@ -0,0 +1,31 @@
+import { FunctionComponent, useState } from "react";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import Logo from "../assets/LogoRH.png"
+
+export const SideBar:FunctionComponent=()=>{
+
+    const [dropDownListOpen, setDropDownListOpen]=useState<boolean>(false)
+
+    return(
+        <aside className="flex flex-col w-1/6 items-center p-3 rounded-r-xl bg-white">
+                <div className="flex items-center">
+                    <img src={Logo} alt="" className="h-20"/>
+                    <h1>Facilita RH</h1>
+                </div>
+                    <ul>
+                        <li className="mt-10 flex items-center text-xl" onClick={()=>setDropDownListOpen(!dropDownListOpen)}><MdOutlineKeyboardArrowDown/> Recrutamento e seleção</li>
+                        {
+                            dropDownListOpen &&
+                            <ul className="mt-5 ml-7 flex flex-col gap-5 ">                    
+                            <li>Gestão de Candidatos
+                            </li>
+                            <li>Triagem e Avaliação</li>
+                            <li>Fluxo de Seleção</li>
+                            <li>Relatórios e Dashboards</li>
+                            </ul>
+                        }
+                        
+                    </ul>
+                </aside>
+    )
+}
