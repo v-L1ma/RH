7e01ff4da6d7e63b9a0ffaac5f2231be32977a71
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-02-24 18:20:58 -0300

    feat: creating the job card

--- RH/src/Routes/GestãoVagas.tsx
+++ RH/src/Routes/GestãoVagas.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import { FaPlus } from "react-icons/fa";
+import CardVaga from "../components/CardVaga";
 
 export const GestaoVagas: FunctionComponent = () => {
   return (
@@ -12,13 +13,23 @@ export const GestaoVagas: FunctionComponent = () => {
           Criar vaga
         </button>
       </div>
-      <div className="bg-white w-full h-5/6 rounded-lg shadow-md shadow-gray-200 p-5">
+
+      <div className="bg-white w-full rounded-lg shadow-md shadow-gray-200 p-5 flex flex-col gap-5">
         <h1 className="text-xl font-bold">Vagas em aberto</h1>
-        <div>
+        <div className="flex gap-7">
             <input type="text" className="border-2 border-black rounded-md p-2 w-full border-opacity-10 "/>
-            <button>Buscar</button>
+            <button className="bg-green-600 px-10 rounded-md">Buscar</button>
         </div>
       </div>
+
+      <div className=" w-full">
+
+          <CardVaga Cargo="Operador de estacionamento" Descriçao="daskjdakjasdk" DataCriacao="24/02/2025" id={1} Localizacao="Santos" Vagas={3} setor="Estacionamento"/>
+
+
+
+      </div>
+
     </div>
   );
 };
--- RH/src/components/CardVaga.tsx
+++ RH/src/components/CardVaga.tsx
@@ -1,25 +1,35 @@
 import { FunctionComponent } from "react"
-import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaUsers } from "react-icons/fa6";
+import { IoMdBriefcase } from "react-icons/io";
 
 interface CardVagaProps{
     id: number;
     Cargo:string | undefined;
     Descriçao:string| undefined;
-    Criador:string| undefined;
+    DataCriacao:string| undefined;
+    Localizacao: string | undefined;
+    Vagas: number | 1;
+    setor: string | undefined;
 }
 
-const CardVaga:FunctionComponent<CardVagaProps> = ({id,Cargo,Descriçao,Criador}) =>{
+const CardVaga:FunctionComponent<CardVagaProps> = ({id,Cargo,DataCriacao, Localizacao, Vagas, setor}) =>{
     return(
-        <div className="bg-white p-5 rounded-md flex shadow-md w-11/12 md:w-4/12" key={id}>
-            <div className="flex flex-col gap-3">
-                <h1 className="text-2xl text-zinc-800 font-extrabold">{Cargo}</h1>
-                <h2>Criado por: {Criador}</h2>
-                <p>{Descriçao}</p>
+        <div className="bg-white p-5 rounded-md flex flex-col shadow-md w-11/12 md:w-6/12" key={id}>
+            <div className="flex justify-between items-center">
+                <p className="bg-green-300 px-3 py-1 rounded-md">Em andamento</p>
+                <BsThreeDotsVertical className="text-xl"/>
             </div>
-            <div className="flex items-end ml-auto pl-3">
-            <p className="text-4xl cursor-pointer">
-                <IoIosArrowDroprightCircle />
-            </p>
+            <div className="flex flex-col gap-3">
+                <h1 className="text-xl text-zinc-800 font-semibold">{Cargo}</h1>
+                <h2>Criada em: {DataCriacao}</h2>
+                
+                <div className="flex gap-5">
+                <p className="flex gap-5"><FaLocationDot/>{Localizacao}</p>
+                <p className="flex gap-5"><FaUsers/>{Vagas} vaga(as)</p>
+                <p className="flex gap-5"><IoMdBriefcase/>{setor}</p>
+                </div>
             </div>
         </div>
     )
