c1d97d17e1715b3e4ecdd6cb2b456e5d35d994db
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-03-13 20:06:48 -0300

    feat: adjusted the side bar

--- RH/src/components/SideBar.tsx
+++ RH/src/components/SideBar.tsx
@@ -1,12 +1,52 @@
-import { FunctionComponent} from "react";
+import { FunctionComponent, useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/LogoRH.png";
-import { LuBriefcaseBusiness } from "react-icons/lu";
-import { RiUserStarLine } from "react-icons/ri";
-import { FaChartBar } from "react-icons/fa";
+import { FiDatabase, FiBriefcase, FiLogOut } from "react-icons/fi";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { FaChevronRight } from "react-icons/fa";
 
 export const SideBar: FunctionComponent = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(true)
   return (
+    <aside className="flex flex-col items-center p-4 rounded-r-xl bg-teal-500 justify-between">
+      <ul className={`flex flex-col gap-3 text-white ${isOpen ? "w-52":"w-12"} transition-all      `}>
+
+        <li className="flex text-2xl hover:bg-white/20 p-3 rounded-lg hover:backdrop-blur-xl items-center gap-3" onClick={()=>setIsOpen(!isOpen)}>
+          <FaChevronRight />
+        </li>
+        <li className="flex text-2xl hover:bg-white/20 p-3 rounded-lg hover:backdrop-blur-xl items-center gap-3">
+          <FiBriefcase />
+          {
+            isOpen && <p className="text-lg">Gestão de vagas</p>
+          }
+        </li>
+        <li className="flex text-2xl hover:bg-white/20 p-3 rounded-lg hover:backdrop-blur-xl items-center gap-3">
+          <FiDatabase />
+          {
+            isOpen && <p className="text-lg">Banco de talentos</p>
+          }
+        </li>
+        <li className="flex text-2xl hover:bg-white/20 p-3 rounded-lg hover:backdrop-blur-xl items-center gap-3">
+          <LuLayoutDashboard />
+          {
+            isOpen && <p className="text-lg">Relatórios</p>
+          }
+        </li>
+      </ul>
+
+      <ul className="flex flex-col gap-3 text-white w-full">
+
+        <li className="flex text-2xl hover:bg-white/20 p-3 rounded-lg hover:backdrop-blur-xl items-center gap-3">
+          <FiLogOut />
+          {
+            isOpen && <p className="text-lg">Sair da conta</p>
+          }
+        </li>
+      </ul>
+    </aside>
+  );
+
+  /*return (
     <aside className="flex flex-col items-center p-10 rounded-r-xl bg-white">
       <div className="flex items-center">
         <img src={Logo} alt="" className="h-20" />
@@ -32,5 +72,5 @@ export const SideBar: FunctionComponent = () => {
         </li>
       </ul>
     </aside>
-  );
+  );*/
 };
