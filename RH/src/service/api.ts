fd7bc5d33bb6595b823dcc6b4dd430c268239afa
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-03-27 08:19:37 -0300

    Update api.ts

--- RH/src/service/api.ts
+++ RH/src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://backend-adotaaipet-production.up.railway.app/'
+    baseURL: 'https://backend-facilitarh-production.up.railway.app/'
   });
 
 export default api;
